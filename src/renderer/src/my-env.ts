acf21ff7b1cb7698e8e4ff2fb45a266e4b6c481a
mkaldev <109039442+manuelkalueka@users.noreply.github.com>
2025-03-19 14:21:36 +0100

    Feat: Update connect-src in index.html to include new API URL for enhanced connectivity

--- src/renderer/src/my-env.ts
+++ src/renderer/src/my-env.ts
@@ -1,3 +1,4 @@
-const BASE_URL = 'https://letruscare.up.railway.app'
-
+// const BASE_URL = 'https://letruscare.up.railway.app' //url dq api
+const BASE_URL = 'https://letruscare-api.onrender.com' //url local
+//'https://letruscare.up.railway.app'
 export { BASE_URL }
