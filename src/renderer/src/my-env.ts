f19cf4a3b9236a2d3dbcf49683ea724293a175db
mkaldev <109039442+manuelkalueka@users.noreply.github.com>
2025-03-19 15:39:52 +0100

    Fix: Update BASE_URL to new API endpoint for improved connectivity

--- src/renderer/src/my-env.ts
+++ src/renderer/src/my-env.ts
@@ -1,4 +1,3 @@
-const BASE_URL = 'https://letruscare.up.railway.app' //url dq api
+const BASE_URL = 'https://letruscare-api.onrender.com' //url dq api
 // const BASE_URL = 'http://localhost:8080' //url local
-//'https://letruscare.up.railway.app'
 export { BASE_URL }
